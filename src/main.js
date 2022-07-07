e75d28c785fe36985b8efe92f51fe64f40fbce47
Guany <1084072690@qq.com>
2022-07-07 22:20:33 +0800

    feat: add axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import '@/assets/styles/base.css'
+import axios from 'axios'
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'https://api.github.com'
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@
 
 <script>
 export default {
-  name: 'Home',
-  components: {}
+  name: 'Home'
 }
 </script>
